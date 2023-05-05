1234a028ca20ecf58ea79fef06d57e2c80579815
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-05-05 15:51:15 +1000

    implement pdf viewer

--- pages/summary/index.tsx
+++ pages/summary/index.tsx
@@ -1,15 +1,20 @@
 import useMyFormContext from "../../components/context/useMyFormContext";
+import MyDocument from "../../components/pdf/MyDocument";
+import {PDFViewer} from "@react-pdf/renderer";
+import {Center} from "@mantine/core";
 
 export default function Summary() {
-    const [formContext, _] = useMyFormContext()
+    const [formState, _] = useMyFormContext()
     return (
         <>
             <h1>
                 Summary
             </h1>
-            <pre>
-                {JSON.stringify(formContext, null, 2)}
-            </pre>
+            <Center>
+                {formState && <PDFViewer width="80%" height="500px">
+                    <MyDocument formState={formState}/>
+                </PDFViewer>}
+            </Center>
         </>
     )
 }
