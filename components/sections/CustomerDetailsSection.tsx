75b43d8b29015a2efe1626864fb4e6a704c0478a
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-05-12 11:14:03 +1000

    create customer details section

---
+++ components/sections/CustomerDetailsSection.tsx
@@ -0,0 +1,31 @@
+import {Dispatch, SetStateAction} from "react";
+import {Button, Stack} from "@mantine/core";
+import MyTextInput from "../forms/MyTextInput";
+import {useForm} from "react-hook-form";
+import MyDatePicker from "../forms/MyDatePicker";
+import {Tabbable} from "../../utils/interfaces/Tabbable";
+
+interface CustomerDetailsSectionProps extends Tabbable {
+  customerDetailsStateSetter: Dispatch<SetStateAction<any>>
+}
+
+export default function CustomerDetailsSection({customerDetailsStateSetter, tabbable = true}: CustomerDetailsSectionProps) {
+  const {control, handleSubmit} = useForm()
+
+  const onClick = handleSubmit(data => {
+    console.log(data)
+    customerDetailsStateSetter(data)
+  })
+
+  const tabIndex = tabbable ? 0 : -1
+
+  return (
+    <Stack>
+      <MyTextInput tabIndex={tabIndex} name="firstName" label="First Name" control={control} required/>
+      <MyTextInput tabIndex={tabIndex} name="lastName" label="Last Name" control={control} required/>
+      <MyTextInput tabIndex={tabIndex} name="email" label="Email" control={control} required/>
+      <MyDatePicker tabIndex={tabIndex} name="birthdate" label="Birth Date" control={control} required/>
+      <Button tabIndex={tabIndex} onClick={onClick}>Next</Button>
+    </Stack>
+  )
+}
