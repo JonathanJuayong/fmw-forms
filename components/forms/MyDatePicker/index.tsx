fe2ad349ee7135aca936ecaea56604192da9492c
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-05-12 11:13:51 +1000

    create date picker form

---
+++ components/forms/MyDatePicker/index.tsx
@@ -0,0 +1,21 @@
+import {MyFormComponentProps} from "../../../utils/interfaces/MyFormComponentProps";
+import {Controller} from "react-hook-form";
+import {Input} from "@mantine/core";
+
+export default function MyDatePicker({name, label, required = false, control, tabIndex}: MyFormComponentProps) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={{required}}
+      render={({field}) => (
+        <Input.Wrapper label={label} {...field}>
+          <Input
+            tabIndex={tabIndex}
+            type="date"
+          />
+        </Input.Wrapper>
+        )}
+    />
+  )
+}
